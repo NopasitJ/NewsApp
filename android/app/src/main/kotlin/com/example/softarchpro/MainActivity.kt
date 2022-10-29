6b3438950db83ef86938cedf1430cb9fcc55cac6
XZ0T1Cz <Nopasit_j@hotmail.co.th>
2022-10-29 23:25:57 +0700

    Userpage

---
+++ android/app/src/main/kotlin/com/example/softarchpro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.softarchpro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/userprofilepage.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class UserProfilePage extends StatefulWidget{
+  
+  @override
+  _UserProfilePageState createState() => _UserProfilePageState();
+
+}
+
+class _UserProfilePageState extends State<UserProfilePage>{
+
+
+  String avatarURL = "https://i.pinimg.com/originals/d1/1e/20/d11e20d44501e1a59439b5344e07f5d7.jpg";
+  Widget build(BuildContext context){
+    
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        elevation: 0.0,
+        title: Text("KMITL News", 
+        style: TextStyle(
+          color:Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+          ),
+        ),
+        
+      ),
+      body: Center(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            CircleAvatar(radius: 56,
+            backgroundImage: NetworkImage(avatarURL),
+            ),
+            SizedBox(
+              height: 12.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("Thanapum Chaipunna",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+            ),
+            IconButton(
+              iconSize: 15,
+              icon: Icon(Icons.edit, color: Colors.white),
+              onPressed: () => print("HELLO")
+            ),
+              ],
+            ),
+            
+            SizedBox(
+              height: 12.0,),    
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Divider(
+                color: Colors.red,
+                height: 18.0,
+                thickness: 0.5,
+              ),
+            ),
+            Expanded(
+              child: Container(),)        
+          ],
+          
+        )
+        
+      ),
+    );
+    
+  }
+
+}
